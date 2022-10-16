70f7093afe05c8125c7c6e0b7b17257241adb561
Dominic Farolino <domfarolino@gmail.com>
2022-10-15 22:47:07 -0400

    Initial wrapping (no unwrapping, and no scope)

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::io;
+use std::io::Read;
+use std::io::Seek;
+use std::io::SeekFrom;
+
+// Adapted from the web version of the original rewrapper
+// (https://github.com/domenic/rewrapper).
+
+mod rewrapper;
+
+fn read_file(filename: &str) -> Result<(File, String), io::Error> {
+    let mut file =
+        OpenOptions::new().read(true).write(true).append(false).open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok((file, contents))
+}
+
+fn write_file(mut file: File, contents: String) -> Result<u8, io::Error> {
+    // Will always work because `file` is opened for writing.
+    file.set_len(0)?;
+    file.seek(SeekFrom::Start(0))?;
+    file.write_all(contents.as_bytes())?;
+    Ok(0)
+}
+
+fn main() {
+    let filename: &str = "source";
+    let column_length: u8 = 100;
+    let (file, file_as_string): (File, String) = match read_file(filename) {
+        Ok((file, string)) => {
+            println!("Successfully read file '{}'", filename);
+            (file, string)
+        },
+        Err(error) => panic!("Error opening file '{}': {:?}", filename, error),
+    };
+
+    let lines: Vec<&str> = file_as_string.split("\n").collect();
+
+    // Initiate unwrapping/rewrapping.
+    let wrapped_lines = rewrapper::wrap_lines(&lines, column_length);
+
+    // Join all lines and write to file.
+    let file_as_string = wrapped_lines.join("\n");
+    match write_file(file, file_as_string) {
+        Ok(_) => println!("Write succeeded"),
+        Err(error) => panic!("Error writing file '{}': {:?}", filename, error),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use test_generator::test_resources;
+    #[test_resources("testcases/*.in.html")]
+    fn verify_resource(input: &str) {
+        assert!(std::path::Path::new(input).exists());
+        let output = input.replace("in.html", "out.html");
+        assert!(std::path::Path::new(&output).exists());
+
+        let (_in_file, in_string) = read_file(input).unwrap();
+        let (_out_file, out_string) = read_file(&output).unwrap();
+
+        let lines: Vec<&str> = in_string.split("\n").collect();
+
+        // Initiate unwrapping/rewrapping.
+        let wrapped_lines = rewrapper::wrap_lines(&lines, 100);
+        let file_as_string: String = wrapped_lines.join("\n");
+        assert_eq!(file_as_string, out_string);
+    }
+}
+
---
+++ src/rewrapper.rs
@@ -0,0 +1,51 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+// TODO: Implement unwrapping, to run before wrapping.
+
+pub fn wrap_lines(lines: &Vec<&str>, column_length: u8) -> Vec<String> {
+    println!("- - The Great Rewrapper - -");
+    println!("We're dealing with {} lines total", lines.len());
+
+    let mut rewrapped_lines: Vec<String> = Vec::new();
+    for line in lines.iter() {
+        if line.len() <= column_length.into() {
+            rewrapped_lines.push(line.to_string());
+        } else {
+            rewrapped_lines.append(&mut wrap_single_line(&line, column_length));
+        }
+    }
+
+    rewrapped_lines
+}
+
+fn wrap_single_line(line: &str, column_length: u8) -> Vec<String> {
+    lazy_static!{
+       static ref REGEX: Regex = Regex::new(r"^(\s*)").unwrap();
+    }
+
+    let mut return_lines = Vec::<String>::new();
+    let indent = REGEX.captures(line).unwrap();
+    let indent: &str = &indent[1];
+    let line = line.trim_start();
+
+    let mut words = line.split(" ");
+    // This will never panic; even if `line` is empty after we trim it, the
+    // split collection will contain a single empty string. See
+    // https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=1035caa5a7a4324272c8966d36d323b4.
+    let mut current_line = String::from(indent) + words.next().unwrap();
+    for word in words {
+        if current_line.len() + 1 + word.len() <= column_length.into() {
+            current_line.push_str(&(" ".to_owned() + word));
+        } else {
+            if current_line != indent {
+                return_lines.push(current_line);
+            }
+            current_line = String::from(indent);
+            current_line.push_str(word);
+        }
+    }
+
+    return_lines.push(current_line);
+    return_lines 
+}
